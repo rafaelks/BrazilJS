dd07e080c39f94f2f3e0b8500cda4981e029e4c4
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2013-07-02 13:28:30 -0300

    Start getting Talks from WS

--- Resources/ui/ListViewTalks.js
+++ Resources/ui/ListViewTalks.js
@@ -1,7 +1,20 @@
 var ListViewTalks = function(dict) {
 	var UI = require("/lib/UI/UI");
+	var RowTalk = require("/lib/UI/TableViewRow").talk;
+	var WSRequest = require("/lib/WS/WSRequest");
 
+	var rows = [];
 	var self = UI.createTableView();
+
+	self.addEventListener("open", function() {
+		WSRequest.getTalks(function(data) {
+			data.forEach(function(talk) {
+				rows.push( new RowTalk(talk) );
+			});
+			self.setData(rows);
+		});
+	});
+
 	return self;
 };
 
