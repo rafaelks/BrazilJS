e14bf4909b20c5ec15c0b645f4fc273329bb000a
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2013-07-02 13:05:03 -0300

    Remove unused requests

--- Resources/lib/WS/WSRequest.js
+++ Resources/lib/WS/WSRequest.js
@@ -1,19 +1,8 @@
 var WS = require("/lib/WS/WS");
 
-exports.getTalks = function(dict) {
+exports.getTalks = function(callback) {
 	WS._request({
-		url: "Products/referencedByCard/" + id,
-		type: "GET",
-		onload: function(response) {
-			callback( response.data );
-		}
-	});
-};
-
-
-exports.getSpeakers = function(dict) {
-	WS._request({
-		url: "Products/referencedByCard/" + id,
+		url: "talks",
 		type: "GET",
 		onload: function(response) {
 			callback( response.data );
