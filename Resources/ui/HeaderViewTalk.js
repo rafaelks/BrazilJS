d5b645b641b0915b80681f75b1c19fa83397b678
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2013-08-08 17:15:49 -0300

    Add Talk Time to details

--- Resources/ui/HeaderViewTalk.js
+++ Resources/ui/HeaderViewTalk.js
@@ -3,9 +3,11 @@ var HeaderViewTalk = function(dict) {
 
 	var UI = require("/lib/UI/UI");
 	var i18n = require("/lib/i18n/Remote");
+	var TiDate = require("/lib/TiDate/TiDate");
 
 	var self = UI.createView({
-		height: Ti.UI.SIZE
+		height: Ti.UI.SIZE,
+		layout: "vertical"
 	});
 
 	self.add( UI.createLabel({
@@ -14,6 +16,16 @@ var HeaderViewTalk = function(dict) {
 		left: 15,
 		right: 15,
 		text: i18n.getValue(obj.name),
+		top: 20
+	}) );
+
+	self.add( UI.createLabel({
+		color: "#666",
+		font: { fontSize: 15 },
+		height: Ti.UI.SIZE,
+		left: 15,
+		right: 15,
+		text: TiDate.getDateFormattedWithoutYear(obj.startAt) + " - " + TiDate.getHours(obj.startAt),
 		top: 20
 	}) );
 
