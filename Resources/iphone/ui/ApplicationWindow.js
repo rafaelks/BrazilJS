a0007ec872df36ba7f4310b4fed18f5ffaa07440
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2013-08-08 17:16:00 -0300

    Register devices for Push

--- Resources/iphone/ui/ApplicationWindow.js
+++ Resources/iphone/ui/ApplicationWindow.js
@@ -24,6 +24,40 @@ var ApplicationWindow = function() {
 	tabLocation.setWindow(winLocation);
 	self.addTab(tabLocation);
 
+	// Push Notification
+	Ti.Network.registerForPushNotifications({
+		types: [
+			Ti.Network.NOTIFICATION_TYPE_ALERT,
+			Ti.Network.NOTIFICATION_TYPE_SOUND
+		],
+		success: function(e) {
+			var deviceToken = e.deviceToken;
+			var WSRequest;
+
+			if (!Ti.App.Properties.hasProperty("push_registered")) {
+				WSRequest = require("/lib/WS/WSRequest");
+				WSRequest.registerPushToken(deviceToken, function() {
+					Ti.App.Properties.setBool("push_registered", true);
+				});
+			}
+		},
+		callback: function(e) {
+			// Show alert to user with message
+			Ti.UI.createAlertDialog({
+				title: "BrazilJS 2013",
+				message: e.data.alert
+			}).show();
+
+			Ti.UI.iPhone.setAppBadge(0);
+		},
+		error: function(e) {
+			Ti.UI.createAlertDialog({
+				title: "BrazilJS 2013",
+				message: e.error
+			}).show();
+		}
+	});
+
 	return self;
 };
 
